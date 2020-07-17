a4008bd40e07de1e3831c6504926ca5d5794b750
Ian Morrill <ianmorrill@icloud.com>
2020-07-17 11:40:16 -0400

    Make model wrapper

---
+++ model_wrapper.py
@@ -0,0 +1,48 @@
+import numpy as np
+from facenet_models import FacenetModel
+
+_model = FacenetModel()
+
+def feed_mtcnn(image, threshold=0.8):
+  """Feeds the model an image get boxes for faces with certain proababilities
+
+  Parameters
+  ----------
+  image: np.ndarray
+    Image to feed to MTCNN
+
+  threshold: float
+    Probability at which include boxes
+
+  Returns
+  -------
+  boxes: np.ndarray
+    A shape (N, 4) array for each box over the threshold
+  """
+  global _model
+
+  boxes, probabilities, _ = _model.detect(image)
+
+  boxes = boxes[[
+    i for i in range(probabilities.size) if probabilities[i] > threshold
+  ]]
+
+  return boxes
+
+def compute_fingeprints(image, boxes):
+  """Fingeprint faces in `boxes` in `image`
+
+  Parameters
+  ----------
+  image: np.ndarray
+    Image to feed to MTCNN
+
+  boxes: np.ndarray
+    Boxes in the image
+
+  Returns
+  -------
+  fingeprints: np.ndarray
+    Fingerprints in np.ndarray of shape (N, 512)
+  """
+  return _model.compute_descriptors(image, boxes)
