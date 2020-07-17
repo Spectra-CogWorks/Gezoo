b8e7067f8e41b6ee4cc8774e4410738edb48f065
Rachna <rachgupta200@gmail.com>
2020-07-16 23:20:35 -0500

    Update input.py

---
+++ input.py
@@ -0,0 +1,7 @@
+def take_picture():
+    #Implementation
+def import_image(file_path):
+    #Implementation
+def import_folder(folder_path):
+    #folder = Path(folder_path).glob("**/*.png")
+    #Implementation
