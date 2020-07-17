0e0ea210e49c852d0c2a4338843a3b1eaa08ee59
jekthewarrior <pranav16@outlook.com>
2020-07-17 12:14:58 -0700

    Made an outline of commands that may be in use in the CLI

--- __init__.py
+++ __init__.py
@@ -1,3 +1,27 @@
 import click as ck
 from pathlib import Path
 
+"""
+Plan
+----
+Update the database
+Initialize the database
+Identify faces in single image
+Use whispers on folder of images
+"""
+
+@ck.command():
+def update_database():
+	pass
+
+@ck.command()
+def find_faces():
+	pass
+	
+@ck.command()
+def initialize_database():
+	pass
+
+@ck.command()
+def run_whispers():
+	pass
