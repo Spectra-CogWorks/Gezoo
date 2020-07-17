8345d706b666cc86db26a52fd3cd3a68a45fd1b7
jekthewarrior <pranav16@outlook.com>
2020-07-16 20:55:00 -0700

    Created incomplete whispers()

--- determine_matches.py
+++ determine_matches.py
@@ -1,1 +1,9 @@
 def determine_match():
+	"""
+	"""
+	pass
+
+def cosine_distance(d1, d2):
+	"""
+	"""
+	return 34.0
--- whispers_algorithm.py
+++ whispers_algorithm.py
@@ -1,1 +1,112 @@
-"whispers algorithm"
+import numpy as np
+from pathlib import Path
+import random
+
+import node as nd
+import model_wrapper as mw
+from determine_matches import cosine_distance
+
+def create_graph(folder_path):
+    """
+    Creates a list of nodes, imports images, and fills the nodes with their respective information
+    
+    Parameters
+    ----------
+    folder_path : str
+        The path to a folder containing solely images
+        
+    
+    
+    Returns
+    -------
+    graph : List[Node]
+        This is a list of the initialized and filled nodes
+    """
+    # graph = []
+    
+    # ? How are we going to decide what to import?
+    # folder = Path(folder_path).glob("**/*.png")
+    
+    # with open() as file:
+        
+    
+    # return graph
+    pass
+    
+def whispers(graph, threshold, max_iterations, weighted_edges):
+    """
+    Using the graph, creates an adjacency matrix which details a relationship between the nodes, aka "edges".
+    Randomly selects nodes to then decide and updates the graph to identify clusters.
+    
+    Parameters
+    ----------
+    graph : List[Node]
+        This is a list of all the nodes
+        
+    threshold : float
+        The threshold distance to decide whether two nodes should have an edge
+    
+    max_iterations : 
+        The maximum number of iterations the algorithm should go through before stopping
+        
+    weighted_edges : bool=True
+        The option of using weighted edges in the function. Default value is True
+    
+    Returns
+    -------
+    None
+    """
+    # Create the adjacency matrix
+    adjacency_matrix = np.zeros((len(graph), len(graph)))
+    
+    # Populate the adjacency matrix
+    # If weighted edges is enabled, the matrix will contain values in [0, infinity) instead of [0, 1]
+    for node in graph:
+        for neighbor in node.neighbors:
+            distance = cosine_distance(node.descriptor, graph[neighbor].descriptor)
+            
+            if distance < threshold:
+                adjacency_matrix[node.ID, neighbor] = 1 / (distance ** 2) + 1 if weighted_edges else 1
+                adjacency_matrix[neighbor, node.ID] = 1 / (distance ** 2) + 1 if weighted_edges else 1
+
+    # Selecting random node
+    # Initializing label counts to be able to detect when convergence occurs (i.e. the number of labels stays the same)
+    # ! Ask about the reason for why we need a maximum number of iterations and which types of graphs will never converge
+    # num_labels_count = len(graph)
+    # past_labels_count = [num_labels_count]
+
+    # Randomly selecting a node and then updating its label by finding the neighbor with the highest frequency
+    for i in max_iterations: # pylint: disable=unused-variable
+        node = random.choice(graph)
+        
+        if len(node.neighbors) != 0:
+            
+            # Checking all the neighbors for which has the highest frequency
+            frequencies = [] # list of tuples (neighbor_index, freq)
+            for neighbor_index, neighbor in enumerate(node.neighbors):
+                frequencies.append((neighbor_index, adjacency_matrix[node.ID, neighbor]))
+            
+            # Convert list to np.ndarray to allow slicing
+            frequencies = np.array(frequencies)
+            
+            # Slice frequencies to find the first max frequency
+            max_freq = np.amax(frequencies[:, 1])
+            max_dupl_indices = [] # list of ints
+            
+            # loops through all the frequencies and determines the indices of the max_freq
+            for neighbor_index, freq in frequencies:
+                if freq == max_freq:
+                    # max_dupl_indices contains the indices of the all the neighbors that
+                    # have the max value
+                    max_dupl_indices.append(neighbor_index)
+                    
+            if len(max_dupl_indices) > 1:
+                max_freq_index = random.choice(max_dupl_indices)
+            else:
+                max_freq_index = max_dupl_indices[0]
+            
+            # Updating the label
+            node.label = graph[node.neighbors[max_freq_index]].label
+            # ! Check this after asking about when graphs never converge
+            # num_labels_count -= 1
+            
